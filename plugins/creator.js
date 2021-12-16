ac270295a000634eff64c836c6f7d42264566d69
Lakshitha <69718607+HYPER-MOD@users.noreply.github.com>
2021-12-16 14:51:46 +0000

    New Multidevice Bot By Lakshitha

---
+++ plugins/creator.js
@@ -0,0 +1,17 @@
+const PhoneNumber = require('awesome-phonenumber')
+async function handler(m) {
+                let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+                    + 'VERSION:3.0\n' 
+                    + 'N:;Ilman;;;'
+                    + 'FN:Ilman\n' // full name
+                    + 'ORG:Ilman;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=94713195756:+94 71-319-5756\n' // WhatsApp ID + phone number
+                    + 'END:VCARD'
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Lakshitha', contacts: [{ vcard }] } }, { quoted: m })
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+
+handler.command = /^(owner|creator)$/i
+
+module.exports = handler
